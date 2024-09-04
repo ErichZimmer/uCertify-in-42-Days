f1f0d10e7c4fd47b7eefc7127f2616832e928131
Erich Zimmer <69478071+ErichZimmer@users.noreply.github.com>
2024-09-03 19:24:05 -0500

    Lesson 5 Labs

---
+++ labs_5.1.1/conditional.js
@@ -0,0 +1,11 @@
+let number = 0;
+
+if (number > 0) {
+    console.log("The number " + number + "is positive);
+}
+else if (number < 0) {
+    console.log("The number " + number + "is negative");
+}
+else {
+    comsole.log("The number " + number + " is zero");
+}
